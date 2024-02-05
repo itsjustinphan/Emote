9a3cd7845d75685af6cb92e4672001cef8a5424b
niccolehan <han1223@uw.edu>
2024-02-04 16:29:42 -0800

    Home page

--- demo-app/src/components/Home.js
+++ demo-app/src/components/Home.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { handleScroll } from "./App";
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+// import insideOutLogo from './img/inside-out.jpeg';
+
 
 //Renders the landing page for users
 export default function Home() {
-  return (
-    <div>
-        <header>
-            <div className="landing-headings">
-            <h1>EMOTE.</h1>
-            <p>blank
-            </p>
-            </div>
-        </header>
+ return (
+   <div>
+       <header>
+           <div className="landing-headings">
+           <h1>Emote.</h1>
+           <h2>Take a quiz and see how you are currently feeling.</h2>
+           <h3>DISCLAIMER: This quiz is NOT a diagnosis!</h3>
+           <img src='/img/inside-out.jpeg' className="Home-image" alt="insideOut-logo"></img> 
+    
+       </div>
+       </header>
 
-    </div>
-  )
+
+   </div>
+ )
 }
