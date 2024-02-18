2f84180b2d80accc017f6d9af89514e48dc1ecb0
VincentChenPK <vincent.chen.pk@gmail.com>
2024-02-18 15:00:22 -0800

    add sample quiz questions & render quiz display

--- demo-app/src/components/Quiz.js
+++ demo-app/src/components/Quiz.js
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { handleScroll } from "./App";
 import Button from 'react-bootstrap/Button';
+import questionsData from "../data/questions.json";
 
-//Renders the landing page for users
-export default function About() {
+export default function Quiz() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [responses, setResponses] = useState([]);
+  const questions = questionsData;
+
+  const handleResponseChange = (response) => {
+    setResponses(prevResponses => [...prevResponses, response]);
+  };
+
+  const handleNext = () => {
+    if (currentQuestion < questions.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+    }
+  };
+
   return (
     <div className="full-height">
-        <header>
-            <div className="landing-headings">
-            <h1>QUIZ</h1>
-            <p>blank
-            </p>
-            </div>
-        </header>
+      <header>
+        <div className="landing-headings">
+          <h1>QUIZ</h1>
+        </div>
+      </header>
 
+      {currentQuestion < questions.length && (
+        <div>
+          <p>{questions[currentQuestion].question}</p>
+          <form>
+            {questions[currentQuestion].options.map((option, index) => (
+              <div key={index}>
+                <input type="radio" id={`option_${index}`} name={`question_${currentQuestion}`} value={option} onChange={() => handleResponseChange(option)} />
+                <label htmlFor={`option_${index}`}>{option}</label>
+              </div>
+            ))}
+          </form>
+          <Button onClick={handleNext}>Next</Button>
+        </div>
+      )}
     </div>
-  )
+  );
 }
